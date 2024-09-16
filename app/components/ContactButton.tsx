802ccd328abcf89c9bf9f135fc18da70f1cd93e0
powtzulist <delasanarwin@gmail.com>
2024-09-16 23:11:46 +0800

    [UI] Added landlord page

---
+++ app/components/ContactButton.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const ContactButton = () => {
+  return (
+    <div className="mt-6 py-4 px-6 bg-airbnb hover:bg-airbnbDark transition text-white rounded-xl cursor-pointer">
+      ContactButton
+    </div>
+  );
+};
