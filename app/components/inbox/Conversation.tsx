37f9b268931fdeb79de80693f72d2074e045da3f
powtzulist <delasanarwin@gmail.com>
2024-10-29 02:21:56 +0800

    [UI] Added inbox page

---
+++ app/components/inbox/Conversation.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Conversation = () => {
+  return (
+    <div className="ps-6 py-4 border cursor-pointer border-gray-300 rounded-xl">
+      <p className="mb-6 text-xl">John Doe</p>
+      <p className="text-airbnbDark">Go to Conversation</p>
+    </div>
+  );
+};
